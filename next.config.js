7344562144c8c85071677c30869260a0f86f6acb
viniciuscasarin <casarinvinicius@gmail.com>
2022-01-13 21:42:16 -0300

    feat: deploy

--- next.config.js
+++ next.config.js
@@ -1,3 +1,5 @@
+const isProduction = process.env.NODE_ENV === "production";
+
 const withMDX = require("@next/mdx")({
   extension: /\.mdx?$/,
 });
@@ -5,4 +7,6 @@ const withMDX = require("@next/mdx")({
 module.exports = withMDX({
   pageExtensions: ["js", "jsx", "mdx"],
   target: "serverless",
+  basePath: isProduction ? "/blog-devptbr" : "",
+  assetPrefix: isProduction ? "/blog-devptbr" : "",
 });
