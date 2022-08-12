66586e79fb7a79fd49b58a1ace92326627d9653b
viniciuscasarin <casarinvinicius@gmail.com>
2022-08-11 22:52:42 -0300

    fix: adding blog domain

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import { getPostsMetaData } from "./api/getPostData";
-import { BLOG_TITLE, BLOG_DESCRIPTION } from "../src/constants";
+import { BLOG_TITLE, BLOG_DESCRIPTION, BLOG_DOMAIN } from "../src/constants";
 import Card from "../src/components/Card";
 import Footer from "../src/components/Footer";
 
@@ -19,11 +19,7 @@ export default function Home({ postsData }) {
         <h1 className={styles.title}>
           Bem-vindo ao{" "}
           <span>
-            <a
-              href="https://github.com/viniciuscasarin/blog-devptbr/blob/master/README.md"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
+            <a href={BLOG_DOMAIN} target="_blank" rel="noopener noreferrer">
               {BLOG_TITLE}!
             </a>
           </span>
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,4 @@
 export const BLOG_TITLE = "Brazuka Codes";
 export const BLOG_DESCRIPTION =
   "Conteúdo sobre desenvolvimento focado a comunidade brasileira!";
+export const BLOG_DOMAIN = "brazuka.codes";
