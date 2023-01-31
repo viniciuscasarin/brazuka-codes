473714c95f8730f4738b71f83506daf7da83442c
viniciuscasarin <casarinvinicius@gmail.com>
2023-01-31 13:05:40 -0300

    feat: vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,15 @@
+import { Analytics } from "@vercel/analytics/react";
+
 import "../styles/globals.css";
 import "../styles/dracula.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
 export default MyApp;
